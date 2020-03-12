03b290cd18e8af77468e841a2429def5915eae89
Karishinamdar <61563536+Karishinamdar@users.noreply.github.com>
2020-03-11 21:45:24 -0700

    Add files via upload

---
+++ database/db_connnection.php
@@ -0,0 +1,30 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "Hydro-1";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+//Setup our query 
+$query = "SELECT * FROM $my_table";     
+
+//Run the Query 
+$result = mysql_query($query);
+
+//If the query returned results, loop through 
+// each result 
+
+if($result) 
+    {   while($row = mysql_fetch_array($result))   
+        {     $name = $row["$yourfield"];     
+              echo "Name: " . $name; 
+   } }   
+
+?>
